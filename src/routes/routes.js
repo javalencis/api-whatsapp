77b63ce48ae8571d0e2e1b4e9db659ce80bf37d0
Jonathan Valencia <javalencis@gmail.com>
2024-11-19 14:47:12 -0500

    new webhook

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -11,6 +11,7 @@ const router = Router();
 router.post("/webhook/orders", handleWebhook);
 router.post("/order", approvePayment);
 router.post("/order/cancel", cancelOrderHandler);
+router.get("/webhook/meta", handleWebhookMeta);
 router.post("/webhook/meta", handleWebhookMeta);
 
 export default router;
