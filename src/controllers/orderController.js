17cfd74e789177863fcb3cd7e5f86f5c48ebacfd
Jonathan Valencia <javalencis@gmail.com>
2024-11-19 14:35:27 -0500

    sendestatus webhook

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -73,5 +73,5 @@ export const cancelOrderHandler = async (req, res) => {
 export const handleWebhookMeta = async (req, res) => {
     console.log("webhook");
     console.log(req.body);
-    res.status(200).json({ status: true });
+    res.sendStatus(200);
 };
