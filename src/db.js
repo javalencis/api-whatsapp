c39969f589e0899b548f63754c7a8500504bbc12
Jonathan Valencia <javalencis@gmail.com>
2024-11-21 08:03:39 -0500

    Variable de entorno

--- src/db.js
+++ src/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(
-            "mongodb://mongo:kHkrVGgGSauLrANBmmEjUTLAWGUTtLsS@mongodb.railway.internal:27017"
-        );
+        await mongoose.connect(process.env.MONGO_URL || "");
         console.log("MongoDB connected succesfully");
     } catch (error) {
         console.log("Error connecting to mongoDB: ", error);
